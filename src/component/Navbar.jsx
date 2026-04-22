23074538fd749631ae7a76b7916b414647c90f84
umayer10-AI <mdumayer10@gmail.com>
2026-04-22 08:09:51 +0600

    add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -19,6 +19,12 @@ const Navbar = () => {
             <li>
               <Link href="/about">About</Link>
             </li>
+            <li>
+              <Link href="/auth/signup">SignUp</Link>
+            </li>
+            <li>
+              <Link href="/auth/signin">SignIn</Link>
+            </li>
           </ul>
         </header>
       </nav>
