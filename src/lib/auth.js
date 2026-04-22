637eb23f4919508c659f6131d0d249e752e3ec57
umayer10-AI <mdumayer10@gmail.com>
2026-04-22 07:45:51 +0600

    addd

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,16 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient("mongodb://localhost:27017/database");
+const db = client.db("umayer-database");
 
 export const auth = betterAuth({
     emailAndPassword: { 
         enabled: true, 
     },
-    database: drizzleAdapter(db, {
-        provider: "pg",
+    database: mongodbAdapter(db, {
+        
+        client
     }),
 });
